a8d71c0be434116da80abf7c7589fb4033f05d37
JuanArbeU <juan.arbelaez514@gmail.com>
2023-10-30 16:06:18 -0500

    TypeScript - Counter

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ContadorScreen } from './src/screens/ContadorScreen'
+//import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+
+export const App = () => {
+
+  return (
+    //<HolaMundoScreen/>
+    <ContadorScreen/>
+  )
+}
+
